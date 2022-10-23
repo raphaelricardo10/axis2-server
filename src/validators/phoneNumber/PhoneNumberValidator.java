079975cce66b4fdebb04ad26544e12740ca1b992
Raphael <raphaelricardo10@gmail.com>
2022-10-23 17:46:00 -0300

    Fixed pattern with unescaped parenthesus

--- src/validators/phoneNumber/PhoneNumberValidator.java
+++ src/validators/phoneNumber/PhoneNumberValidator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class PhoneNumberValidator extends Validator{
     public PhoneNumberValidator() {
-        super("^([1-9]{2}) (?:[2-8]|9[1-9])[0-9]{3}-[0-9]{4}$");
+        super("^\\([1-9]{2}\\) (?:[2-8]|9[1-9])[0-9]{3}\\-[0-9]{4}$");
     }
 
     @Override
