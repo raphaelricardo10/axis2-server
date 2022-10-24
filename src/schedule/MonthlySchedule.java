aedbf5dcad23c3c6627a1d262790cd35d8bd0a46
Raphael <raphaelricardo10@gmail.com>
2022-10-24 18:59:13 -0300

    Feat: created a monthly schedule

---
+++ src/schedule/MonthlySchedule.java
@@ -0,0 +1,49 @@
+package schedule;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class MonthlySchedule {
+    private Set<ScheduleDay> days;
+    private Set<DayOfWeek> workingDays;
+    private LocalDate dayIterator;
+    
+    public MonthlySchedule(DayOfWeek[] workingDays) {
+        this.days = new HashSet<ScheduleDay>();
+        this.workingDays = Set.of(workingDays);
+        this.dayIterator = LocalDate.now();
+
+        this.generateSchedule();
+    }
+
+    public Set<ScheduleDay> getDays() {
+        return days;
+    }
+
+    public Set<DayOfWeek> getWorkingDays() {
+        return workingDays;
+    }
+
+    public void setWorkingDays(DayOfWeek[] workingDays) {
+        this.workingDays = Set.of(workingDays);
+    }
+
+    public void generateSchedule() {
+        LocalDate endDate = this.dayIterator.plusDays(30);
+
+        ScheduleInterval workTime = new ScheduleInterval(LocalTime.of(9, 0), LocalTime.of(17, 0));
+        ScheduleInterval lunchTime = new ScheduleInterval(LocalTime.of(12, 0), LocalTime.of(13, 0));
+
+        while(!this.dayIterator.isAfter(endDate)){
+            if(this.workingDays.contains(this.dayIterator.getDayOfWeek())){
+                this.days.add(new ScheduleDay(30, workTime, lunchTime, this.dayIterator));
+            }
+
+            this.dayIterator = this.dayIterator.plusDays(1);
+        }
+    }
+
+}
