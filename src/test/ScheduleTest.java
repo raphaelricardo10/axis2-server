979d23060c38613d1eaa8b953d64d42678722910
Raphael <raphaelricardo10@gmail.com>
2022-10-24 18:59:33 -0300

    Test: added the monthly schedule test

--- src/test/ScheduleTest.java
+++ src/test/ScheduleTest.java
@@ -1,8 +1,11 @@
 package test;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 import junit.framework.TestCase;
+import schedule.MonthlySchedule;
 import schedule.ScheduleDay;
 import schedule.ScheduleInterval;
 
@@ -11,9 +14,17 @@ public class ScheduleTest extends TestCase{
         ScheduleInterval workTime = new ScheduleInterval(LocalTime.of(9, 0), LocalTime.of(17, 0));
         ScheduleInterval lunchTime = new ScheduleInterval(LocalTime.of(12, 0), LocalTime.of(13, 0));
 
-        ScheduleDay day = new ScheduleDay(30, workTime, lunchTime);
+        ScheduleDay day = new ScheduleDay(30, workTime, lunchTime, LocalDate.now());
 
         assertEquals(day.getFreeSlots().size(), 14);
         assertFalse(day.getFreeSlots().contains(lunchTime.getStart()));
+    }
+
+    public void testMonthlyScheduleGenerated() {
+        DayOfWeek[] daysOfWeek = {DayOfWeek.THURSDAY, DayOfWeek.TUESDAY};
+
+        MonthlySchedule schedule = new MonthlySchedule(daysOfWeek);
+
+        schedule.getDays();
     }
 }
