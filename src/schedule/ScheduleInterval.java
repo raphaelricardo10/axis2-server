a8ce01b1571eccc3c611c267de8e0deec425e112
Raphael <raphaelricardo10@gmail.com>
2022-10-24 16:13:25 -0300

    Formatted code

--- src/schedule/ScheduleInterval.java
+++ src/schedule/ScheduleInterval.java
@@ -5,20 +5,24 @@ import java.time.LocalTime;
 public class ScheduleInterval {
     private LocalTime start;
     private LocalTime end;
-    
+
     public ScheduleInterval(LocalTime start, LocalTime end) {
         this.start = start;
         this.end = end;
     }
+
     public LocalTime getStart() {
         return start;
     }
+
     public void setStart(LocalTime start) {
         this.start = start;
     }
+
     public LocalTime getEnd() {
         return end;
     }
+
     public void setEnd(LocalTime end) {
         this.end = end;
     }
