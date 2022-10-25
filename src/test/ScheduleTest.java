1f45409878422b9394974a370750240eb6361dfa
Raphael <raphaelricardo10@gmail.com>
2022-10-25 00:10:39 -0300

    Test: changed to use the DoctorSchedule class

--- src/test/ScheduleTest.java
+++ src/test/ScheduleTest.java
@@ -5,10 +5,11 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import junit.framework.TestCase;
-import schedule.MonthlySchedule;
 import schedule.DailySchedule;
 import schedule.ScheduleInterval;
+import schedule.doctor.DoctorSchedule;
 import workData.DayOfWork;
+import workData.WorkInfo;
 
 public class ScheduleTest extends TestCase {
     public void testSlotsGenerated() {
@@ -27,13 +28,20 @@ public class ScheduleTest extends TestCase {
     public void testMonthlyScheduleGenerated() {
         DayOfWeek[] daysOfWeek = { DayOfWeek.THURSDAY, DayOfWeek.TUESDAY };
 
-        MonthlySchedule schedule = new MonthlySchedule(daysOfWeek, LocalDate.of(2022, 10, 24));
+        int appointmentDuration = 30;
+        ScheduleInterval workTime = new ScheduleInterval(LocalTime.of(9, 0), LocalTime.of(17, 0));
+        ScheduleInterval lunchTime = new ScheduleInterval(LocalTime.of(12, 0), LocalTime.of(13, 0));
 
-        assertEquals(schedule.getDays().size(), 9);
+        DayOfWork dayOfWork = new DayOfWork(appointmentDuration, workTime, lunchTime);
+        WorkInfo workInfo = new WorkInfo(dayOfWork, daysOfWeek);
+
+        DoctorSchedule schedule = new DoctorSchedule(null, workInfo, LocalDate.of(2022, 10, 24));
+
+        assertEquals(schedule.getSchedule().size(), 9);
         
-        for(DailySchedule day : schedule.getDays()) {
+        for(DailySchedule day : schedule.getSchedule()) {
             DayOfWeek dayOfWeek = day.getDate().getDayOfWeek();
-            assertTrue(schedule.getWorkingDays().contains(dayOfWeek));
+            assertTrue(schedule.getWorkInfo().getWorkingDays().contains(dayOfWeek));
         }
     }
 }
