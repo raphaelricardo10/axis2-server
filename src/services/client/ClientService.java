fd2d0e67bb00ef6c44b37aeeacaaad91974513f8
Raphael <raphaelricardo10@gmail.com>
2022-10-31 22:05:55 -0300

    Refactor: changed clients list to map

--- src/services/client/ClientService.java
+++ src/services/client/ClientService.java
@@ -1,30 +1,32 @@
 package services.client;
 
+import java.util.Map;
 import java.util.List;
+import java.util.HashMap;
 import java.util.ArrayList;
 
 import mock.MockData;
 import stub.PersonStub;
 
 public class ClientService {
-    private List<PersonStub> clients;
+    private Map<String, PersonStub> clients;
 
     public ClientService() throws Exception {
         PersonStub person = new PersonStub(MockData.makePerson());
-        this.clients = new ArrayList<PersonStub>();
-        clients.add(person);
+        this.clients = new HashMap<String, PersonStub>();
+        clients.put(person.getCpf(), person);
     }
 
     public PersonStub[] getClients() {
         PersonStub[] clientsArr = new PersonStub[this.clients.size()];
-        this.clients.toArray(clientsArr);
+        this.clients.values().toArray(clientsArr);
 
         return clientsArr;
     }
 
     public PersonStub createClient(String name, String email, String cpf, String gender, String phoneNumber, String birthDate) throws Exception {
         PersonStub client = new PersonStub(name, email, cpf, gender, phoneNumber, birthDate);
-        this.clients.add(client);
+        this.clients.putIfAbsent(client.getCpf(), client);
 
         return client;
     }
